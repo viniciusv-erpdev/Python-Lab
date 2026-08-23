6de8c9137e939c4c05108b6ea9cf831d04e1f0d1
viniciusv-erpdev <vcvicari@gmail.com>
2026-08-23 18:32:22 -0300

    exercício 01 e 02

---
+++ semana_01/exercicio_01.py
@@ -0,0 +1,11 @@
+# Variávieis em Python
+
+nome = 'Vinicius'
+idade = 22
+altura = 1.80
+estudante = True
+
+print('\n' + "meu nome é: " + nome + '\n')
+print("eu tenho: " + f"{idade}" + " anos " + '\n')
+print("Minha altura é: " + f"{altura}" + "metros " + '\n')
+print("Sou estudante: " + f"{estudante}")
---
+++ semana_01/exercicio_02.py
@@ -0,0 +1,7 @@
+# imagine que sua loja vende um produto por R$ 80 e existe um desconto de 15%. Como descobrir o desconto?
+
+preco_produto = 80
+
+valor_produto_com_desconto = preco_produto - (0.15 * preco_produto)
+
+print("o valor final do produto é: " + f"{valor_produto_com_desconto}")
