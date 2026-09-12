9ab4005b88b55c4bf271790aaef55f1583780bfd
viniciusv-erpdev <vcvicari@gmail.com>
2026-09-12 17:46:27 -0300

    update mini project 12/09/2026

--- Mini-Projetos/Gerenciador de tarefas/backend.py
+++ Mini-Projetos/Gerenciador de tarefas/backend.py
@@ -1,5 +1,10 @@
+# Imports
+import os
+from pathlib import Path
+
 # Variáveis
 task_dict = dict()
+file_path = "tasks_list.txt"
 
 def receive_data(description, priority, status):
     """Recebe os dados das tarefas pelos usuários.
@@ -23,21 +28,44 @@ def receive_data(description, priority, status):
     
     return task_list
 
-def create_task_dict(task_list):
+def create_file(file_name):
+
+    '''Cria um novo arquivo 'tasks_list.txt' se ele não existir '''
+
+    if Path(file_name).exists() == False:
+        Path(file_name).touch
+        print(f'Arquivo criado com sucesso! \n')
+
+
+def create_task_dict(file_name, task_list):
     """Cria um dicionário com os dados recebidos do usuário.
 
     Recebe a lista de tarefas do usuário e associa com um id no dicionário
     
     Args:
-        task_list: lista de tarefas
+        file_name: nome do arquivo
         task_dict: dicionário a ser criado
 
     Returns:
         O dicinário de tarefas"""
 
-    id = 1
+    id = 0
 
-    task_dict = dict()
+    if Path('tasks_list.txt').exists():
+        
+        with open(file_name, 'r', encoding='utf-8') as f:
+
+            # Caso o arquivo esteja vazio
+            if os.path.isfile(file_path) and os.path.getsize(file_path) == 0:
+                id = 0
+
+            for line in f:
+
+                id += 1
+
+    else:
+
+        print("Erro o arquivo tasks_list.txt não foi encontrado")
 
     task_dict[id] = task_list
 
@@ -46,11 +74,26 @@ def create_task_dict(task_list):
     
     return task_dict
 
+def update_file(file_name, task_dict):
 
-def write_file(file_name, task_dict):
+    """Adiciona linhas a um arquivo com base no dicionário de dados.
 
-    with open(file_name, 'w', encoding='utf-8') as file:
-        for item in task_dict:
-            file.write(str(task_dict.values()))
+    Recebe o nome do arquivo e o dicionário e escreve um arquivo
+    com os dados do dicionário
+    
+    Args:
+        file_name: Nome do arquivo
+        task_dict: Dicionário de tarefas"""
+
+    with open(file_name, 'a', encoding='utf-8') as file:
+
+        for id, item in task_dict.items():
+
+            # Caso o arquivo esteja vazio
+            if os.path.isfile(file_path) and os.path.getsize(file_path) == 0:
+                file.write(f'{id}, {item}')
+
+            if os.path.isfile(file_path) and os.path.getsize(file_path) != 0:
+                file.write(f'\n{id}, {item}')
 
     pass
--- Mini-Projetos/Gerenciador de tarefas/main.py
+++ Mini-Projetos/Gerenciador de tarefas/main.py
@@ -10,11 +10,13 @@ def main():
     priority = input("Prioridade: ")
     status = input("Status: ")
 
+    backend.create_file(file_name)
+
     task_list = backend.receive_data(description,priority,status)
 
-    task_dict = backend.create_task_dict(task_list)
+    task_dict = backend.create_task_dict(file_name, task_list)
 
-    backend.write_file(file_name, task_dict)
+    backend.update_file(file_name, task_dict)
     
     
 if __name__ == "__main__":
